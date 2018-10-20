d0561863db40738436bd2574a70edddbdf024f9e
Muhammad Alief <alief.pribadi@gmail.com>
2018-10-20 17:51:38 +0700

    my course

--- config.js
+++ config.js
@@ -16,17 +16,17 @@ function get_data_course(){
 			if(response)
 			{
 					for (let index = 0; index < response.length; index++) {
-							$('#ambilCourse').append(
+							$('#ambilCourseHome').append(
 							`<li data-role="list-divider">${response[index].main_course_name}</li>
-							<li><a href="course_detail.html?course_id=${response[index].id}${response[index].course_name}</a></li>`);
+							<li><a href="course_detail.html?course_id=${response[index].id}">${response[index].course_name}</a></li>`);
 					}
-					$("#ambilCourse").listview('refresh');
+          // $('#ambilCourse').trigger('create');
+					$("#ambilCourseHome").listview('refresh');
 			}
 	});
 }
 
-function getDetail(userId)
-{
+function getDetail(userId){
   var link = "https://mcc-odd1819.herokuapp.com/detail_courses?course_id=" + userId;
 
   var opt =
@@ -49,6 +49,51 @@ function getDetail(userId)
   });
 }
 
+function saveCourse(courseId){
+  var data = localStorage.getItem('course');
+
+  var arr;
+
+  if(!data)
+  {
+      arr = [];
+  }
+  else
+  {
+      //karena localStorage tidak bisa simpan array, jadi DATA harus di convert dulu jadi JSON
+      arr = JSON.parse(data);
+  }
+  arr.push(courseId);
+
+  data = JSON.stringify(arr);
+  localStorage.setItem('course',data);
+}
+
+function getMyCourse() {
+
+  var retrievedData = localStorage.getItem("course");
+  var data = JSON.parse(retrievedData);
+
+  for (let index = 0; index < data.length; index++) {
+      // $('#ambilMyCourse').append(
+      // `<li>
+  		// 	<a href="#">${data[index]}</a>
+  		// </li>`);
+      console.log("a");
+  }
+  console.log("a");
+  // $('#ambilCourse').trigger('create');
+  // $("#ambilCourseHome").listview('refresh');
+}
+
+function test() {
+
+  var retrievedData = localStorage.getItem("course");
+  var data = JSON.parse(retrievedData);
+
+  console.log(data);
+
+}
 $(function() {
   //onLoad
 
@@ -66,8 +111,20 @@ $(function() {
   var params = window.location.search;
   var search = new URLSearchParams(params);
   var course_id = search.get('course_id');
-
   getDetail(course_id);
+
+  //add Course
+  $('#btnAddCourse').click(function() {
+    saveCourse(course_id);
+  });
+
+  //test ambil course
+  $('#testCourse').click(function() {
+    test(course_id);
+  });
+
+  //MyCourse
+  getMyCourse();
 })
 
   $('#btnCourse').click(function()
